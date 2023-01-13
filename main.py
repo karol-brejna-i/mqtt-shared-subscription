b3e24f5fdddd2fe84a91457879236f5bac6b1878
karol-brejna-i <karolbrejna@apache.org>
2023-01-13 17:23:50 +0100

    More info on publish

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ logger.info(f"SUBSCRIBE_TOPIC: {SUBSCRIBE_TOPIC}")
 
 def on_connect(client, userdata, flags, rc, costam):
     logger.info("on_connect")
-    logger.info("Connected with {client}, {userdata}, {flags}, {rc}, {costam}")
+    logger.info(f"Connected with {client}, {userdata}, {flags}, {rc}, {costam}")
     # mqtt_client.subscribe(SUBSCRIBE_TOPIC)
 
 
@@ -48,7 +48,8 @@ mqtt_client.loop_start()
 logger.info("after loop start")
 
 connect_result = mqtt_client.connect(MQTT_HOST, port=MQTT_PORT, keepalive=60)
-logger.info(f"connect_result: {connect_result}")
+logger.info(f"connect_result: {connect_result} - type: {type(connect_result)}")
+logger.debug("MQTT_ERR_SUCCESS = 0")
 # mqtt_client.connect_async()
 
 app = FastAPI()
@@ -59,6 +60,7 @@ def tick():
     # mqtt_client
     result = mqtt_client.publish(MQTT_TOPIC, "tick", qos=1)
     logger.info(f"publish result: {result}")
+    logger.debug("MQTT_ERR_SUCCESS = 0, MQTT_ERR_NO_CONN = 4")
     # mqtt_client.single(topic, payload=None, qos=0, retain=False, hostname="localhost",
     #        port=1883, client_id="", keepalive=60, will=None, auth=None, tls=None,
     #        protocol=mqtt.MQTTv311, transport="tcp")
